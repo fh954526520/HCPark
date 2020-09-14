2788e539cd5ec349be0480561bc14ff2ea62ca83
xue <1071679845@qq.com>
2020-09-14 09:12:13 +0800

    测试

---
+++ pages/orderList/order.vue
@@ -0,0 +1,173 @@
+
+<template>
+    <view class="content">
+		<view class="header-content">
+			<view class="left-nav" @click="orderChange(1)">
+					<text v-bind:class="{'border-show': border ? true:false}" style="width: auto;height: 100%;display: flex;justify-content: center;align-items: center;">所有订单</text>
+			</view>
+			<view class="middle-nav"></view>
+			<view class="right-nav" @click="orderChange(2)">
+					<text v-bind:class="{'border-show': border ? false:true}" style="width: auto; height: 100%;	display: flex;justify-content: center;align-items: center;">待支付</text>
+			</view>
+		</view>
+		<view class="all-orders" v-if="allOrder">
+			<view class="order-items-content">
+				<view class="items" v-for="(item,index) in 15" :key="index">
+					<view class="items-left">
+						<view style="margin-left: 5vw;display: flex;flex-direction: column;">
+							<text v-bind:style="{color:IFpay ? '#FF3300':''}">{{moeny}}元</text>
+							<text style="font-size: 24rpx;color: #E4E4E4;">{{address}}</text>
+						</view>
+					</view>
+					<view class="items-right">
+						<view>
+							<text>{{date}}</text>
+							<text style="margin-left: 2vw;">{{times}}</text>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="tobepaid-orders" v-else>
+			<view class="order-items-content">
+				<view class="items" v-for="(item,index) in 15" :key="index">
+					<view class="items-left">
+						<view style="margin-left: 5vw;display: flex;flex-direction: column;">
+							<text v-bind:style="{color:IFpay ? '#FF3300':''}">{{moeny}}元</text>
+							<text style="font-size: 24rpx;color: #E4E4E4;">{{address}}</text>
+						</view>
+					</view>
+					<view class="items-right">
+						<view>
+							<text>{{date}}</text>
+							<text style="margin-left: 2vw;">{{times}}</text>
+						</view>
+					</view>
+				</view>
+			</view>
+		</view>
+    </view>
+</template>
+
+<script>
+	export default {
+	    data() {
+	        return {
+	           allOrder:true,
+			   border:true,
+			   moeny:7,
+			   address:"合川区xx万华路",
+			   IFpay:true,
+			   date:"19-09-03",
+			   times:"15-27-03"
+	        }
+	    },
+	    methods: {
+	        orderChange:function(val){
+				var _self=this;
+				if(val===1){
+					_self.border=true;
+					_self.allOrder=true;
+				}
+				if(val===2){
+					_self.border=false;
+					_self.allOrder=false;
+				}
+				
+				
+				
+			},
+	        
+	    }
+	}
+</script>
+
+<style scoped>
+	page{
+		width: 100%;
+		height: 100%;
+	}
+	.content{
+		height: 100%;
+		width: 100%;
+		background-color: #F3F3F3;
+	}
+	.header-content{
+		height: 7vh;
+		width: 100%;
+		background-color:#FFFFFF ;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color:#666666;
+	}
+	.all-orders{
+		height:91vh;
+		width: 100%;
+		margin-top: 2vh;
+		background-color:#FFFFFF;
+	}
+	.tobepaid-orders{
+		height: 91vh;
+		width: 100%;
+		margin-top: 2vh;
+		background-color: #FFFFFF;
+	}
+	.left-nav{
+		width: 49.75%;
+		height: 100%;
+		float: left;
+		font-size: 3vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.middle-nav{
+		width: 0.5%;
+		float: left;
+		background-color:#666666;
+		height: 80%;
+	}
+	.right-nav{
+		width: 49.75%;
+		height: 100%;
+		float: left;
+		font-size: 3vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.border-show{
+		border-bottom: 2px solid #4482FF;
+		color: #4482FF;
+	}
+	.order-items-content{
+		width: 100%;
+		height: 100%;
+		overflow-y: auto;
+	}
+	.items{
+		width: 100%;
+		height: 10vh;
+		border: #E4E4E4;
+		border-bottom: solid 1px #CCCCCC;
+	}
+	.items-left{
+		width: 50%;
+		height: 100%;
+		float: left;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	.items-right{
+		width: 50%;
+		height: 100%;
+		display: flex;
+		justify-content: center;
+		font-size: 3.5vw;
+		align-items:center;
+		color: #666666;
+		font-weight:500; 
+	}
+</style>
---
+++ pages/payfortips/payfortips.vue
@@ -0,0 +1,121 @@
+<template>
+	<view class="content">
+		<view class="top-image">
+			<image src="../../static/image/paydetails/che拷贝4@2x.png" style="width: 120rpx;height: 75rpx;" mode=""></image>
+		</view>
+		<view class="input-parking-content">
+			<text class="font-edi">输入车位号</text>
+			<view class="carinput-input">
+				<view class="carinput-input-i" :class="{'input-active':carIndex == i}" @tap="inputKey" :data-index="i" v-for="(item,i) in 8" :key="i" >
+					1
+				</view>
+			</view>
+		</view>
+		<view class="button-group">
+				<button type="default" class="btn" @click="confirm">确定</button>
+				<button type="default" class="btn" @click="cancel" style="margin-top: 3vh;">取消</button>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				carIndex:-1,
+			}
+		},
+		methods:{
+			confirm:function(){
+				console.log("点击了确定按钮");
+			},
+			cancel:function(){
+				console.log("点击了取消按钮");
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	page{
+		height: 100%;
+		width: 100%;
+	}
+	.content{
+		height:100vh;
+		width: 100%;
+	}
+	.top-image{
+		width: 100%;
+		height: 20vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.input-parking-content{
+		width:96vw;
+		height: 20vh;
+		margin-left:2vw;
+		border-radius: 24rpx;
+		box-shadow: 0px 0px 5px #8bc3ff;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.font-edi{
+		font-size: 36rpx;
+		color: #237FEC;
+		letter-spacing: 8rpx;
+		margin-top: 2vh; 
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.button-group{
+		width: 100%;
+		display: flex;
+		margin-top: 8vh;
+		flex-direction: column;
+	}
+	.btn{
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 38rpx;
+		width: 70vw;
+		border-radius: 60rpx;
+		background-color: #4482FF;
+		color: #FFFFFF;
+	}
+	.carinput-input {
+	    display: flex;
+	    justify-content: center;
+		height: 5vh;
+		margin-top: 2vh;
+	}
+	
+	.carinput-input-i {
+	    width: 50upx;
+	    border-bottom-width: 2upx !important;
+	    border-bottom-style: solid;
+	    border-bottom-color: #BCBCBC;
+	    font-size: 36upx;
+	    color: #333333;
+	    line-height: 1;
+	    margin-right: 12upx;
+	    padding-bottom: 7upx;
+	    text-align: center
+	}
+	
+	.carinput-input-i:nth-last-child(1) {
+	    margin-right: 0
+	}
+	
+	.carinput-input .input-active {
+	    border-bottom-width: 2upx !important;
+	    border-bottom-style: solid;
+	    border-bottom-color: #23CCAB;
+	    color: #23CCAB
+	}
+</style>
